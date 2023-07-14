11bc146badd1e519da1cf3c398630f513da55762
Sebastian Sierra <jusebastian16@gmail.com>
2023-07-14 00:31:19 -0400

    Sebastian

--- app.py
+++ app.py
@@ -26,3 +26,22 @@ def load_model(model_name):
     model_class = model_dict[model_name]
     model = model_class(weights="imagenet")
     return model
+
+# Define image size
+IMAGE_SIZE = (224, 224)
+
+# Function to make predictions
+def predict(image, model, top_k=3):
+    st.info("Resizing image...")
+    image = ImageOps.fit(image, IMAGE_SIZE, Image.LANCZOS)
+    image = np.asarray(image)
+    image = preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+    st.info("Predicting image...")
+    prediction = model.predict(image)
+    decoded_predictions = decode_predictions(prediction, top=top_k)[0]
+    return decoded_predictions
+
+# Function to get a random joke
+def get_random_joke():
+    return pyjokes.get_joke()
